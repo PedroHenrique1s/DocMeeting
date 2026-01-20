e870dc24ddf07ef0dd535638f4b26844e2c4612f
PedroHenrique1s <Phsouzahd15@gmail.com>
2026-01-19 22:27:41 -0300

    refactor(app): Desenvolvimento dos botões para acessar com email e autenticação do google

--- DocMeeting/src/app/app.ts
+++ DocMeeting/src/app/app.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './services/auth';
 import { UploadArquivo } from './components/upload-arquivo/upload-arquivo';
 
 @Component({
   selector: 'app-root',
-  imports: [UploadArquivo],
+  imports: [UploadArquivo, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected title = 'DocMeeting';
+  currentUser$;
+  credits = 0;
+
+  constructor(private authService: AuthService) {
+    // 2. Atribua o valor AQUI dentro. Agora o authService já existe.
+    this.currentUser$ = this.authService.user$;
+
+    // Monitora o usuário
+    this.authService.user$.subscribe(async (user) => {
+      if (user) {
+        const profile = await this.authService.getProfile();
+        this.credits = profile?.credits ?? 0;
+      } else {
+        this.credits = 0;
+      }
+    });
+  }
+
+  loginGoogle() {
+    this.authService.signInWithGoogle();
+  }
+
+  loginEmail() {
+    alert('Funcionalidade de E-mail será implementada no próximo passo!');
+  }
+
+  logout() {
+    this.authService.signOut();
+  }
 }
