cde646ea88db862a6fbfbb2c5238a10eb9184579
PedroHenrique1s <Phsouzahd15@gmail.com>
2026-01-26 21:29:48 -0300

    feat(loading): Desenvolvimento de load dinâmico para carregamento de dados vindo do banco de dados

---
+++ DocMeeting/src/app/components/loading/loading.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading',
+  imports: [CommonModule],
+  templateUrl: './loading.html',
+  styleUrl: './loading.scss',
+})
+export class Loading {
+  @Input() message: string = 'Carregando seus dados...';
+}
