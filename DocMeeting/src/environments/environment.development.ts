dffd8a45131417108c25ef2fe5fc6d49fa16c489
PedroHenrique1s <Phsouzahd15@gmail.com>
2026-02-12 21:44:48 -0300

    config(enviroment): TConfiguração da api keys pelo vercel

---
+++ DocMeeting/src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiKey: 'PROCESSED_BY_VERCEL',
+  production: false,
+  supabaseUrl: 'PROCESSED_BY_VERCEL',
+  supabaseKey: 'PROCESSED_BY_VERCEL',
+};
---
+++ DocMeeting/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  apiKey: 'PROCESSED_BY_VERCEL',
+  production: true,
+  supabaseUrl: 'PROCESSED_BY_VERCEL',
+  supabaseKey: 'PROCESSED_BY_VERCEL',
+};
